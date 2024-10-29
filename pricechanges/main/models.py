ce2d4919db9e01629764de56f0e694a6d78882d5
Astspace <asteroid.space@gmail.com>
2024-10-29 16:49:37 +0300

    29.10.2024

--- pricechanges/main/models.py
+++ pricechanges/main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Items(models.Model):
+    id_wb = models.CharField(max_length=30)
+    name = models.CharField(max_length=255)
+    brand = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    rating = models.FloatField()
+    feedbacks = models.IntegerField()
+    volume = models.IntegerField()
+    time_create = models.DateTimeField(auto_now_add=True)
