63f1e9bd37149359f8be4261b6d3c0cb4f35d710
Astspace <asteroid.space@gmail.com>
2024-11-26 17:00:59 +0300

    26.11.2024

--- pricechanges/users/urls.py
+++ pricechanges/users/urls.py
@@ -6,5 +6,7 @@ app_name = 'users'
 
 urlpatterns = [
     path('login/', views.LoginUsers.as_view(), name='login'),
-    path('logout/', views.logout_users, name='logout')
+    path('logout/', views.logout_users, name='logout'),
+    path('register/', views.RegisterUser.as_view(), name='register'),
+    path('profile/', views.ProfileUser.as_view(), name='profile'),
     ]
