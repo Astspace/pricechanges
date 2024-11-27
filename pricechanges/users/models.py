449c7694cc3ae90673e0db154bd6aba4b0b35673
Astspace <asteroid.space@gmail.com>
2024-11-27 16:55:35 +0300

    fix users 27.11.2024

--- pricechanges/users/models.py
+++ pricechanges/users/models.py
@@ -1,5 +1,2 @@
-from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-class User(AbstractUser):
-    birthday = models.DateTimeField(blank=True, null=True, verbose_name='Дата рождения')
