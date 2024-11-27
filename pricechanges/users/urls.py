ff5bbf1185c930ade6d8e7537dac886d1f184d70
Astspace <asteroid.space@gmail.com>
2024-11-27 16:51:24 +0300

    27.11.2024

--- pricechanges/users/urls.py
+++ pricechanges/users/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path
+from django.contrib.auth.views import PasswordChangeDoneView, PasswordChangeView, PasswordResetView, \
+    PasswordResetConfirmView, PasswordResetDoneView, PasswordResetCompleteView
+from django.urls import path, reverse_lazy
 from . import views
-
+from .forms import UserPasswordChangeForm
 
 app_name = 'users'
 
@@ -9,4 +11,29 @@ urlpatterns = [
     path('logout/', views.logout_users, name='logout'),
     path('register/', views.RegisterUser.as_view(), name='register'),
     path('profile/', views.ProfileUser.as_view(), name='profile'),
-    ]
+    path('password-change/', PasswordChangeView.as_view(form_class=UserPasswordChangeForm,
+                                                        success_url=reverse_lazy("users:password_change_done"),
+                                                        template_name="users/password_change_form.html"),
+         name="password_change"),
+    path('password-change/done/', PasswordChangeDoneView.as_view(template_name="users/password_change_done.html"),
+         name="password_change_done"),
+    path('password-reset/',
+         PasswordResetView.as_view(
+             template_name="users/password_reset_form.html",
+             email_template_name="users/password_reset_email.html",
+             success_url=reverse_lazy("users:password_reset_done")
+         ),
+         name='password_reset'),
+    path('password-reset/<uidb64>/<token>/',
+         PasswordResetConfirmView.as_view(
+             template_name="users/password_reset_confirm.html",
+             success_url=reverse_lazy("users:password_reset_complete")
+         ),
+         name='password_reset_confirm'),
+    path('password-reset/done/',
+         PasswordResetDoneView.as_view(template_name="users/password_reset_done.html"),
+         name='password_reset_done'),
+    path('password-reset/complete/',
+         PasswordResetCompleteView.as_view(template_name="users/password_reset_complete.html"),
+         name='password_reset_complete'),
+]
